dd56288ea682eb546eec84f77419e868e5e4845f
jannatTumpa <tumpa.1432@gmail.com>
2016-01-11 01:56:08 +0600

    task 2 partial

--- index.php
+++ index.php
@@ -86,8 +86,8 @@ $app->get('/active', function() use ($app){
 	$size_data=count($data);
 	//echo $size_data;
 	$response = [];
-	//parse the data to get active hour
 	
+	//parse the data to get active hour
 	 if($time_span == "day"){
 		 $tweetArray=[];
 		 $maxCount=0;
@@ -139,21 +139,122 @@ $app->get('/active', function() use ($app){
 		   }
 		 $maxCount=max($hourCount);
 		 //echo $maxCount;
+		 //finding maximum frequency
 		 for($k=0;$k<24;$k++){
 			if($hourCount[$k]==$maxCount)
 				$response["$k"] = $maxCount;
-		 }
-		 
-		 
+		 }  
 	  }
 	
 	//$response["answer"]= $data;
 	//echo $data;
 	//echo "I am here";
 	echoRespnse(200, $response);
-
 });
 
+$app->get('/hashtag', function() use ($app){
+	
+	//get the request
+	$req = $app->request();
+	$screen_name = $req->get('twitter_id');
+	$n=$req->get('n');
+	
+	//$name_start = strrpos($srch, 'id=') + 1; // +1 so we don't include the space in our result
+	//$name_end = strrpos($srch, ' ')+1;
+	//$name_width= name_end-name_start;
+	//echo $name_width;
+	//$cityname = substr($question, $last_word_start);
+	//echo $screen_name;
+	
+	
+	//build signature
+	$oauth_hash = '';
+	$oauth_hash .= 'count=200&';
+	$oauth_hash .= 'oauth_consumer_key=DwqmZvelWwN6DERiteqUuFbip&';
+	$oauth_hash .= 'oauth_nonce=' . time() . '&';
+	$oauth_hash .= 'oauth_signature_method=HMAC-SHA1&';
+	$oauth_hash .= 'oauth_timestamp=' . time() . '&';
+	$oauth_hash .= 'oauth_token=2860541348-EH8MJm85ffjfEtRpltqHnkNudlmmD2UMS32zP2U&';
+	$oauth_hash .= 'oauth_version=1.0&';
+	$oauth_hash .= 'screen_name='. $screen_name ;
 
+	$base = '';
+	$base .= 'GET';
+	$base .= '&';
+	$base .= rawurlencode('https://api.twitter.com/1.1/statuses/user_timeline.json');
+	$base .= '&';
+	$base .= rawurlencode($oauth_hash);
+	$key = '';
+	$key .= rawurlencode('g5YF0YUHjELSKAwoaCYTroBMVCwzdbTyQhGTQdvnB4sIVvhBhq');
+	$key .= '&';
+	$key .= rawurlencode('8pzJoo9ZaJVGU6ZFCnxJR43WSK55A1Grzg655FpGffm7R');
+	$signature = base64_encode(hash_hmac('sha1', $base, $key, true));
+	$signature = rawurlencode($signature);
+	
+	
+	//build cUrl header
+	$oauth_header = '';
+	$oauth_header .= 'count="200", ';
+	$oauth_header .= 'oauth_consumer_key="DwqmZvelWwN6DERiteqUuFbip", ';
+	$oauth_header .= 'oauth_nonce="' . time() . '", ';
+	$oauth_header .= 'oauth_signature="' . $signature . '", ';
+	$oauth_header .= 'oauth_signature_method="HMAC-SHA1", ';
+	$oauth_header .= 'oauth_timestamp="' . time() . '", ';
+	$oauth_header .= 'oauth_token="2860541348-EH8MJm85ffjfEtRpltqHnkNudlmmD2UMS32zP2U", ';
+	$oauth_header .= 'oauth_version="1.0", ';
+	$oauth_header .= 'screen_name="' . $screen_name . '"';
+	$curl_header = array("Authorization: Oauth {$oauth_header}", 'Expect:');
+
+	// hitting URL
+	$curl_request = curl_init();
+	curl_setopt($curl_request, CURLOPT_HTTPHEADER, $curl_header);
+	curl_setopt($curl_request, CURLOPT_HEADER, false);
+	curl_setopt($curl_request, CURLOPT_URL, 'https://api.twitter.com/1.1/statuses/user_timeline.json?count=200&screen_name=' .$screen_name . "");
+	curl_setopt($curl_request, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl_request, CURLOPT_SSL_VERIFYPEER, false);
+	$json = curl_exec($curl_request);
+	curl_close($curl_request); 
+	$data = json_decode($json);
+	
+	$size_data=count($data);
+	$temp=[];
+	$response = [];
+	$resultArray=[];
+	
+	for($i=0;$i<$size_data;$i++){
+		$countTag=count($data[$i]->entities->hashtags);
+		for($j=0;$j<$countTag;$j++)
+			  $temp[]= $data[$i]->entities->hashtags[$j]->text;
+			  //echo $temp;
+	}
+	for($i=0;$i<count($temp);$i++){
+		$count=1;
+		//echo $temp[$i];
+		for($j=0;$j<$i;$j++){
+			
+			if($temp[$j]==$temp[$i]){
+				//$a=$temp[$j];
+				$count++;
+			}	
+		}
+		$response["$temp[$i]"]=$count;
+			
+	}
+	// usort($response,function($a,$b){
+		// if($a<$b){
+			// $temp1=$response[$""]
+		// }
+	//});
+	// $resultArray = $resultArray.sort(function (a, b) {
+    // return a.val.localeCompare( b.val );
+	// });
+	// $response=json_decode($resultArray);
+
+
+	//$resultArray = array_slice($response, 0, $n); 
+	// foreach ($response as $key => $value) {
+    // echo "$key: $value\n";
+	echoRespnse(200, $response);
+});
 
 $app->run();
